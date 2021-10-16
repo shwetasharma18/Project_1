f30cee4a22db1b0866f8ca44a22143b2fd5bb71e
shwetasharma18 <shweta18@navgurukul.org>
2021-10-16 17:14:04 +0530

    project inst

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
+
+// this all will be the database for the current project with in the same file. 
+//In further concept we will introduce some other topics like(file system)so then we will use seperate file for the data.
+
+//add some more employee details to the disctionary, to experiment with the application efficiently.
+
+var employeesList=[
+    {
+        name:"kumar nayak",
+        id:1,
+        state:"Andhra Pradesh",
+        city:"Bangalore",
+        level:1
+    },
+    {
+        name:"Rahit",id:2, state:"Delhi",city:"Dharamshala",level:1
+    },
+    {
+        name:"Praveen kumar",id:3, state:"Mumbai",city:"Pune",level:2
+    }
+]
+
+
 // here you need to write the functions related to the project.
+
+//Function1
+
+//write a function getInputData which will do subtasks mention below:
+//Get the input1 value whatever the user is entering for the task by using getElementById
+//Store that value in a variable 
+//And with the help of the showListOfData function, it depicts the details according to your input which you have stored, by passing it as a parameter in the function.
+
+// add function1 here below ......
+
+
+
+
+//Function2
+
+//Make a function showListOfData which will do the following:
+//Have to create the table to depict the data so take a variable to show make it
+//Get the html element by using getElementById to show it as a table
+//Create the headings of the table
+//As we don’t want everything because filter will return us a list
+//Use object destructuring to get the particular name element because we have to search it by name
+//The list is returning whole information and we have to show the details of the employee as well. So by using the object destructuring process only to place all the elements in the table.
+//Even if the user is searching in upper case, just fix only one case which your computer will understand (Can use toLowerCase function for this)
+//We have to show the table on the page so read about the .innerHtml link mentioned above.
+//Make sure we have to give input again and again
+
+
+// add function2 here below .........
+
+
+
+
+//Function3
+//Create an arrow function to show the salaries and levels of employees with the help of Switch case.
+//You have to mention the salaries according to the level only inside the print statement of the switch cases.
+
+// add the function3 here below ........
+
